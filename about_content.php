7b4a45061a93d6449138fbaad53fe836c93320ec
Ashwin Sajiv <sajivashwin@gmail.com>
2018-05-10 21:54:30 +1000

    Minor changes to about

--- about_content.php
+++ about_content.php
@@ -1,7 +1,7 @@
 
 <div class="alert alert-info"><strong>About US</strong></div>
 <div class="about">
-<p>Mobile dog grooming is a start-up initiated by Tom, which is aiming at providing convenient grooming services for customer’s dogs, at their own place. This report documents the procedures for project management concerning the software development for the Mobile Dog Grooming business.
+<p>Mobile dog grooming is a start-up initiated by Tom, which is aiming at providing convenient grooming services for customer’s dogs, at their own place. This website provides services for the project, Mobile Dog Grooming business.
 This project is undertaken by AO11_Group4 team, a group of students of The University of Melbourne. The team is responsible for delivering the software for the project proposed by the business owner, Tom. </p>
 
 </div>
